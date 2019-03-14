e83c59353f7d99e5a4ba699552adbc0b210d86b4
KashF <Kashfarhadi@gmail.com>
2019-03-14 13:32:53 -0400

    finished login page design

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -15,11 +15,16 @@ import {
 import logo from "../logo.svg";
 
 class LoginForm extends Component {
-  state = { username: "", password: "" };
+  state = {
+    username: "",
+    password: ""
+  };
 
-  handleLogin = e => {
-    e.preventDefault();
-    this.props.login(this.state);
+  handleLogin = event => {
+    this.props.login({
+      username: this.state.username,
+      password: this.state.password
+    });
   };
 
   handleChange = e => {
@@ -29,8 +34,85 @@ class LoginForm extends Component {
   render() {
     const { isLoading, err } = this.props;
     return (
-      <React.Fragment>
-        <h1>Login</h1>
+      <Grid
+        textAlign="center"
+        style={{ height: "100%" }}
+        verticalAlign="middle"
+      >
+        <Grid.Column style={{ maxWidth: 450 }}>
+          <div />
+          <Header as="h1" color="blue" textAlign="center">
+            Behold, Kwitter has Risen!
+          </Header>
+          <Image src={logo} size="medium" centered />
+          <Form onSubmit={this.handleLogin} size="large">
+            <Segment stacked color="grey">
+              <Form.Field
+                label="Username:"
+                required
+                placeholder="Username"
+                type="text"
+                control={Input}
+                autofocus
+                onChange={this.handleChange}
+              />
+              <Form.Field
+                htmlFor="password"
+                label="Password:"
+                required
+                placeholder="Password"
+                type="password"
+                control={Input}
+                onChange={this.handleChange}
+              />
+              <Button.Group>
+                {/* redirect */}
+                {/* connected react router */}
+                <Button
+                  type="submit"
+                  disabled={isLoading}
+                  onClick={this.handleLogin}
+                  positive
+                  size="large"
+                  to="/profile"
+                >
+                  Login now you Sucker!
+                </Button>
+                <Button.Or />
+                {/* <Link to="/RegisterForm"> 
+                  <Button size= "large" color="blue">
+                    You're not on Kwitter yet son? Register Here!
+                     </Button>
+                   </Link> */}
+              </Button.Group>
+              <div> {this.props.result} </div>
+            </Segment>
+          </Form>
+          {isLoading && <Spinner name="circle" color="blue" />}
+          {err && <p style={{ color: "red" }}>{err}</p>}
+        </Grid.Column>
+      </Grid>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    result: state.loginResult
+  };
+};
+const mapDispatchToProps = dispatch => {
+  return {
+    login: loginData => dispatch(login(loginData))
+  };
+};
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(LoginForm);
+
+/* 
+<h1>Login</h1>
         <form onSubmit={this.handleLogin}>
           <label htmlFor="username">Username</label>
           <input
@@ -57,11 +139,4 @@ class LoginForm extends Component {
     );
   }
 }
-
-export default connect(
-  ({ auth }) => ({
-    isLoading: auth.loginLoading,
-    err: auth.loginError
-  }),
-  { login }
-)(LoginForm);
+*/
